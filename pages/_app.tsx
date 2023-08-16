c52ec05a6ab43c0b8b649dc16773cc40ece2102e
sham40 <142365972+sham40@users.noreply.github.com>
2023-08-16 07:18:17 +0530

    Webpage.html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+<html>
+<head>
+     <title>web page</title>
+</head>
+<body>
+    <front color="blue" size="15" face="arial">
+    Blue Arial Font 
+    </font>
+    HTML<hr size="4" color ="red">
+    CSS
+    <ol>
+    <li>Sham</li>
+    <li>Abishek</li>
+    <li>Thiloyathan</li>
+    <ol>
+</body>
+</html>
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
